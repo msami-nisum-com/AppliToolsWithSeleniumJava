ff8aa1416a1aa115c532b909a9ad5105c6b5b84c
Muhammad Sami Shaikh <msami@nisum.com>
2024-12-31 20:34:27 +0500

    init

--- src/test/java/TestCase2.java
+++ src/test/java/TestCase2.java
@@ -1,3 +1,4 @@
+import com.applitools.eyes.BatchInfo;
 import com.applitools.eyes.RectangleSize;
 import com.applitools.eyes.selenium.Eyes;
 import com.applitools.eyes.selenium.fluent.Target;
@@ -7,21 +8,23 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-public class TestCase {
+public class TestCase2 {
     static WebDriver driver;
+    static BatchInfo myBatch;
     Eyes eyes;
     @BeforeAll
     public static void beforeAll() {
+        myBatch = new BatchInfo("My First Batch");
         driver = WebDriverManager.chromedriver().create();
     }
     @BeforeEach
     public void beforeEach(TestInfo testInfo) {
         eyes = new Eyes();
+        eyes.setBatch(myBatch);
         eyes.setApiKey("2tCeC9wT2TC4iUj9cSa101NkYK6G5DsnyNpc101SOES106cwQ110");
         eyes.open(
                 driver,
-                "My First Tests",
+                "My Second Tests",
                 testInfo.getTestMethod().get().getName(),
                 new RectangleSize(1000,600)
         );
@@ -33,7 +36,8 @@ public class TestCase {
     }
     @Test
     public void myTestCaseTwo() {
-        driver.get("https://applitools.com/helloworld/?diff1");
+        driver.get("https://applitools.com/helloworld/");
+//        driver.get("https://applitools.com/helloworld/?diff1");
         eyes.check(Target.window());
     }
 
